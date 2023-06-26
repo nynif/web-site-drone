e5d7c88a87debf5e2caa61fa9691a6236a49eb17
nynif <nynif@proton.me>
2023-06-26 11:42:56 +0200

    add robot sitemap, adapt content et alt att for seo

--- main/static/map.js
+++ main/static/map.js
@@ -1,6 +1,9 @@
 const copy = "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a>";
 const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-const layer = L.tileLayer(url, { attribution: copy });
+const layer = L.tileLayer(url, { attribution: copy }).on('tileload', function(e) {
+    e.tile.alt = 'Carte interactive de la région SUD PACA France ';
+});
+
 const map = L.map("map", { 
     layers: [layer],
     center: [43.297559, 5.391289],
